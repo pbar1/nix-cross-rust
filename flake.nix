c8333d881a959e46ab2c03a4e60815351804dfe6
Pierce Bartine <piercebartine@gmail.com>
2024-11-14 22:37:33 -0800

    nixfmt

--- flake.nix
+++ flake.nix
@@ -5,18 +5,21 @@
     naersk.url = "github:nix-community/naersk/master";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      naersk,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
       in
       {
         defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
       }
     );
 }
